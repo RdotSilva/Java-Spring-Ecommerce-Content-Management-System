3ed6f38d39338213bd4702fd60907282cc40f6f9
RdotSilva <RyandotSilva@gmail.com>
2020-03-01 21:29:00 -0500

    Add validation error messages

--- src/main/java/com/rdotsilva/cms/ecommerce/models/data/Page.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/models/data/Page.java
@@ -3,6 +3,7 @@ package com.rdotsilva.cms.ecommerce.models.data;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="pages")
@@ -14,8 +15,13 @@ public class Page {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    // @Size is used for validation
+    @Size(min = 2, message = "Title must be at least 2 characters long")
     private String title;
     private String slug;
+
+    @Size(min = 5, message = "Content must be at least 5 characters long")
     private String content;
     private int sorting;
 }
