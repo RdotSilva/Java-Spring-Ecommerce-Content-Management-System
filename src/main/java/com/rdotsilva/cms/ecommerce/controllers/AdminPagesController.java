2b1c104aa2af5a2490c14b466949f0c9051fa6a5
RdotSilva <RyandotSilva@gmail.com>
2020-03-01 21:29:59 -0500

    Add page crud methods

--- src/main/java/com/rdotsilva/cms/ecommerce/controllers/AdminPagesController.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/controllers/AdminPagesController.java
@@ -2,12 +2,14 @@ package com.rdotsilva.cms.ecommerce.controllers;
 
 import com.rdotsilva.cms.ecommerce.models.data.Page;
 import com.rdotsilva.cms.ecommerce.models.data.PageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -15,11 +17,14 @@ import java.util.List;
 @RequestMapping("/admin/pages")
 public class AdminPagesController {
 
+    @Autowired
     private PageRepository pageRepository;
 
-    public AdminPagesController(PageRepository pageRepository) {
-        this.pageRepository = pageRepository;
-    }
+    // By using @Autowired above, we can comment out this code. Instead the pageRepository will be automatically
+    // injected into code as needed.
+//    public AdminPagesController(PageRepository pageRepository) {
+//        this.pageRepository = pageRepository;
+//    }
 
     @GetMapping
     public String index(Model model) {
@@ -34,10 +39,103 @@ public class AdminPagesController {
     }
 
     @GetMapping("/add")
-    public String add(Model model) {
+    public String add(@ModelAttribute Page page) {
+
         // Create a new model to pass into add.html
         // This is one way of doing it you can also use @ModelAttribute annotation as shown above
-        model.addAttribute("page", new Page());
+//        model.addAttribute("page", new Page());
         return "admin/pages/add";
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        Page pageCurrent = pageRepository.getOne(page.getId());
+
+        // Check page for errors
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("pageTitle", pageCurrent.getTitle());
+            return "admin/pages/add";
+        }
+
+        // Display Page added if successful
+        redirectAttributes.addFlashAttribute("message", "Page Added");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        // Construct page slug
+        String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug().toLowerCase().replace(" ", "-");
+
+        Page slugExists = pageRepository.findBySlug(slug);
+
+        // Check if slug exists, if so then display errors, otherwise set the new page slug and save repo
+        if (slugExists != null) {
+            redirectAttributes.addFlashAttribute("message", "Slug already exists, choose another slug");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("page", page);
+        } else {
+            page.setSlug(slug);
+            page.setSorting(100);
+
+            pageRepository.save(page);
+        }
+
+        // Redirect when done
+        return "redirect:/admin/pages/add";
+    }
+
+    // Edit page, id is passed in from @PathVariable annotation below
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+
+        Page page = pageRepository.getOne(id);
+
+        model.addAttribute("page", page);
+
+        return "admin/pages/edit";
+    }
+
+    @PostMapping("/edit")
+    public String edit(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        // Check page for errors
+        if (bindingResult.hasErrors()) {
+            return "admin/pages/edit";
+        }
+
+        // Display Page added if successful
+        redirectAttributes.addFlashAttribute("message", "Page Edited");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        // Construct page slug
+        String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug().toLowerCase().replace(" ", "-");
+
+        Page slugExists = pageRepository.findBySlugAndIdNot(slug, page.getId());
+
+        // Check if slug exists (OUTSIDE OF THE CURRENT PAGE), if so then display errors, otherwise set the new page slug and save repo
+        if (slugExists != null) {
+            redirectAttributes.addFlashAttribute("message", "Slug already exists, choose another slug");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("page", page);
+        } else {
+            page.setSlug(slug);
+
+            pageRepository.save(page);
+        }
+
+        // Redirect when done
+        return "redirect:/admin/pages/edit/" + page.getId();
+    }
+
+    // Delete a page
+    @GetMapping("/delete/{id}")
+    public String edit(@PathVariable int id, RedirectAttributes redirectAttributes) {
+
+        Page page = pageRepository.getOne(id);
+        pageRepository.deleteById(id);
+
+        redirectAttributes.addFlashAttribute("message", page.getTitle() + " page deleted!");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        return "redirect:/admin/pages";
     }
 }
