f5fc53c0ff6068f40911f4cb56090597834a1591
RdotSilva <RyandotSilva@gmail.com>
2020-03-02 19:39:29 -0500

    Add new find methods

--- src/main/java/com/rdotsilva/cms/ecommerce/models/data/PageRepository.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/models/data/PageRepository.java
@@ -7,4 +7,19 @@ import java.util.List;
 // Page is the page Entity, Integer is the int ID value
 public interface PageRepository extends JpaRepository<Page, Integer> {
 
+    // Custom query creation. Works by naming method in a very specific way.
+    // Check Spring Data JPA documentation for more info.
+    Page findBySlug(String slug);
+
+    // Custom query example
+    // Find slug outside of current page
+//    @Query("SELECT p FROM Page p WHERE p.id != :id and p.slug = :slug")
+//    Page findBySlugNotOnPage(int id, String slug);
+
+    // This does same as custom query above just much neater
+    Page findBySlugAndIdNot(String slug, int id);
+
+    // Find all Pages sorted
+    List<Page> findAllByOrderBySortingAsc();
+
 }
