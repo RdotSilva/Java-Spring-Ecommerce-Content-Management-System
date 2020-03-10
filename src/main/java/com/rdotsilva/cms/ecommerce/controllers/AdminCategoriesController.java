ed631441247cf5529a9b7f9ec985cd034616449d
RdotSilva <RyandotSilva@gmail.com>
2020-03-10 14:49:16 -0400

    Add add post method

--- src/main/java/com/rdotsilva/cms/ecommerce/controllers/AdminCategoriesController.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/controllers/AdminCategoriesController.java
@@ -2,13 +2,17 @@ package com.rdotsilva.cms.ecommerce.controllers;
 
 import com.rdotsilva.cms.ecommerce.models.CategoryRepository;
 import com.rdotsilva.cms.ecommerce.models.data.Category;
+import com.rdotsilva.cms.ecommerce.models.data.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -37,5 +41,38 @@ public class AdminCategoriesController {
     public String add(Category category) {
 
         return "admin/categories/add";
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid Category category, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        // Check page for errors
+        if (bindingResult.hasErrors()) {
+            return "admin/categories/add";
+        }
+
+        // Display Category added if successful
+        redirectAttributes.addFlashAttribute("message", "Category Added");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        // Construct category slug
+        String slug = category.getName().toLowerCase().replace(" ", "-");
+
+        Category categoryExists = categoryRepository.findByName(category.getName());
+
+        // Check if category exists, if so then display errors, otherwise set the new category slug and save repo
+        if (categoryExists != null) {
+            redirectAttributes.addFlashAttribute("message", "Category already exists, choose another slug");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("categoryInfo", category);
+        } else {
+            category.setSlug(slug);
+            category.setSorting(100);
+
+            categoryRepository.save(category);
+        }
+
+        // Redirect when done
+        return "redirect:/admin/categories/add";
     }
 }
