4508e00315703ac008924a09ab6cc64552df8f52
RdotSilva <RyandotSilva@gmail.com>
2020-03-10 14:31:19 -0400

    Add ModelAttribute sharing example

--- src/main/java/com/rdotsilva/cms/ecommerce/controllers/AdminCategoriesController.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/controllers/AdminCategoriesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +26,12 @@ public class AdminCategoriesController {
 
         return "admin/categories/index";
     }
+
+    // Use this example if you want a model attribute accessible to all methods with the name category
+//    @ModelAttribute("category")
+//    public Category getCategory() {
+//        return new Category();
+//    }
 
     @GetMapping("/add")
     public String add(Category category) {
