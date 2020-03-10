75e4cf850bc3afb42adba5513eefd26be6c11971
RdotSilva <RyandotSilva@gmail.com>
2020-03-10 15:31:33 -0400

    Add findAllByOrderBySortingAsc method

--- src/main/java/com/rdotsilva/cms/ecommerce/models/CategoryRepository.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/models/CategoryRepository.java
@@ -3,7 +3,11 @@ package com.rdotsilva.cms.ecommerce.models;
 import com.rdotsilva.cms.ecommerce.models.data.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
     Category findByName(String name);
+
+    List<Category> findAllByOrderBySortingAsc();
 }
