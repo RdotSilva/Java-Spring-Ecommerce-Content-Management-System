9df79047e6613610c54c549f605ccd473b7020f2
RdotSilva <RyandotSilva@gmail.com>
2020-03-12 16:12:23 -0400

    Add Product class fields

--- src/main/java/com/rdotsilva/cms/ecommerce/models/data/Product.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/models/data/Product.java
@@ -1,13 +1,46 @@
 package com.rdotsilva.cms.ecommerce.models.data;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
 @Data
 public class Product {
-    
+
+    // Use @ID to mark as primary key
+    // Use @GeneratedValue for auto increment ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String slug;
+
+    private String description;
+
+    private String image;
+
+    private String price;
+
+    private String quantity;
+
+    // Use @Column annotation when the column ID is different than the field
+    @Column(name = "category_id")
+    private String categoryId;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
 }
