cdbf539dc311499ac511496a38ba1f731e7a2411
RdotSilva <RyandotSilva@gmail.com>
2020-03-12 16:22:17 -0400

    Add products index get method

--- src/main/java/com/rdotsilva/cms/ecommerce/controllers/AdminProductsController.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/controllers/AdminProductsController.java
@@ -1,9 +1,14 @@
 package com.rdotsilva.cms.ecommerce.controllers;
 
 import com.rdotsilva.cms.ecommerce.models.ProductRepository;
+import com.rdotsilva.cms.ecommerce.models.data.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/products")
@@ -11,4 +16,13 @@ public class AdminProductsController {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        List<Product> products = productRepository.findAll();
+
+        model.addAttribute("products", products);
+
+        return "admin/products/index";
+    }
 }
