ea6ccb13bf51bdfab0e0d0451fa133f9917c9665
RdotSilva <RyandotSilva@gmail.com>
2020-03-12 16:07:52 -0400

    Add Product data model class

---
+++ src/main/java/com/rdotsilva/cms/ecommerce/models/data/Product.java
@@ -0,0 +1,13 @@
+package com.rdotsilva.cms.ecommerce.models.data;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+    
+}
