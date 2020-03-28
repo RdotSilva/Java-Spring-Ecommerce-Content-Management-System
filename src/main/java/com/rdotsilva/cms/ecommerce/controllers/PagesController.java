b53923e1dae43c01c5f21c1cf85a87f2e8378d4a
RdotSilva <RyandotSilva@gmail.com>
2020-03-28 17:51:28 -0400

    Add PagesController class

---
+++ src/main/java/com/rdotsilva/cms/ecommerce/controllers/PagesController.java
@@ -0,0 +1,12 @@
+package com.rdotsilva.cms.ecommerce.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PagesController {
+
+    
+}
