0eb7aa545839b4e2dcb54a1fdc728695df28efc9
RdotSilva <RyandotSilva@gmail.com>
2020-03-28 18:21:27 -0400

    Add sharedData method

--- src/main/java/com/rdotsilva/cms/ecommerce/Common.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/Common.java
@@ -1,7 +1,25 @@
 package com.rdotsilva.cms.ecommerce;
 
+import com.rdotsilva.cms.ecommerce.models.PageRepository;
+import com.rdotsilva.cms.ecommerce.models.data.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
 
 @ControllerAdvice
 public class Common {
+
+    @Autowired
+    private PageRepository pageRepository;
+
+    @ModelAttribute
+    public void sharedData(Model model) {
+
+        List<Page> pages = pageRepository.findAllByOrderBySortingAsc();
+
+        model.addAttribute("cpages");
+    }
 }
