d7ef145f4757f3b6de603e2fd2f748c003a76784
RdotSilva <RyandotSilva@gmail.com>
2020-03-28 17:55:49 -0400

    Add @GetMapping for home

--- src/main/java/com/rdotsilva/cms/ecommerce/controllers/PagesController.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/controllers/PagesController.java
@@ -5,6 +5,7 @@ import com.rdotsilva.cms.ecommerce.models.data.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +15,7 @@ public class PagesController {
     @Autowired
     private PageRepository pageRepository;
 
+    @GetMapping
     public String home(Model model) {
 
         Page page = pageRepository.findBySlug("home");
