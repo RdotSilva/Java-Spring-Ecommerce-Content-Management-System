8e038228fc12ff7e5e53b55e908031a70070a3a0
RdotSilva <RyandotSilva@gmail.com>
2020-03-29 15:16:54 -0400

    Add cart info to Common

--- src/main/java/com/rdotsilva/cms/ecommerce/Common.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/Common.java
@@ -1,5 +1,6 @@
 package com.rdotsilva.cms.ecommerce;
 
+import com.rdotsilva.cms.ecommerce.models.Cart;
 import com.rdotsilva.cms.ecommerce.models.CategoryRepository;
 import com.rdotsilva.cms.ecommerce.models.PageRepository;
 import com.rdotsilva.cms.ecommerce.models.data.Category;
@@ -9,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
 
 @ControllerAdvice
@@ -21,11 +24,31 @@ public class Common {
     private CategoryRepository categoryRepository;
 
     @ModelAttribute
-    public void sharedData(Model model) {
+    public void sharedData(Model model, HttpSession session) {
 
         List<Page> pages = pageRepository.findAllByOrderBySortingAsc();
 
         List<Category> categories = categoryRepository.findAll();
+
+        boolean cartActive = false;
+
+        // Check for active shopping cart data
+        if (session.getAttribute("cart") != null) {
+            HashMap<Integer, Cart> cart = (HashMap<Integer, Cart>)session.getAttribute("cart");
+
+            int itemsInCart = 0;
+            double total = 0;
+
+            for (Cart value : cart.values()) {
+                itemsInCart += value.getQuantity();
+                total += value.getQuantity() * Double.parseDouble(value.getPrice());
+            }
+
+            model.addAttribute("csize", itemsInCart);
+            model.addAttribute("ctotal", total);
+
+            cartActive = true;
+         }
 
         model.addAttribute("cpages", pages);
         model.addAttribute("ccategories", categories);
