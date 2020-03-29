eeab749d652473bc432f78d81e10e70a1f7adaf0
RdotSilva <RyandotSilva@gmail.com>
2020-03-29 15:00:04 -0400

    Add cart product add method

--- src/main/java/com/rdotsilva/cms/ecommerce/controllers/CartController.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/controllers/CartController.java
@@ -1,9 +1,27 @@
 package com.rdotsilva.cms.ecommerce.controllers;
 
+import com.rdotsilva.cms.ecommerce.models.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/cart")
 public class CartController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/add/{id}")
+    public String add(@PathVariable int id, HttpSession session, Model model) {
+
+        return null;
+
+    }
+
 }
