22f57588515208851ee5167fe82aa484e5b5ddfb
RdotSilva <RyandotSilva@gmail.com>
2020-03-29 15:08:58 -0400

    Add check for existing cart

--- src/main/java/com/rdotsilva/cms/ecommerce/controllers/CartController.java
+++ src/main/java/com/rdotsilva/cms/ecommerce/controllers/CartController.java
@@ -1,6 +1,8 @@
 package com.rdotsilva.cms.ecommerce.controllers;
 
+import com.rdotsilva.cms.ecommerce.models.Cart;
 import com.rdotsilva.cms.ecommerce.models.ProductRepository;
+import com.rdotsilva.cms.ecommerce.models.data.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/cart")
+@SuppressWarnings("unchecked")
 public class CartController {
 
     @Autowired
@@ -19,6 +23,18 @@ public class CartController {
 
     @GetMapping("/add/{id}")
     public String add(@PathVariable int id, HttpSession session, Model model) {
+
+        Product product = productRepository.getOne(id);
+
+        // Check for current session is set
+        if (session.getAttribute("cart") == null) {
+
+            // Create new cart and add first item
+            HashMap<Integer, Cart> cart = new HashMap<>();
+            cart.put(id, new Cart(id, product.getName(), product.getPrice(), 1, product.getImage()));
+            session.setAttribute("cart", cart);
+
+        } 
 
         return null;
 
